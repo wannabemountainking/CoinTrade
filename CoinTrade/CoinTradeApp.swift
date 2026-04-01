652b39ec16fd0e9699209a92bcea31447bbc308b
김도윤 <kdy0096@gmail.com>
2026-04-02 01:28:41 +0900

    Initial Commit

---
+++ CoinTrade/CoinTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTradeApp.swift
+//  CoinTrade
+//
+//  Created by yoonie on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct CoinTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
