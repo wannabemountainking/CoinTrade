f32f9ebe4d8026501ac357533d6c677741248ada
wannabemountainking <kdy0096@gmail.com>
2026-04-02 16:46:15 +0900

    MainView

--- CoinTrade/CoinTradeViewModel.swift
+++ CoinTrade/CoinTradeViewModel.swift
@@ -10,22 +10,76 @@ import Combine
 import Observation
 
 
+enum TradeError: String, Error {
+    case priceError = "거래 가격 정보가 없습니다"
+}
+
 @Observable
 final class CoinTradeViewModel {
     
     let ds: DataService
-    var coinTrades: [CoinTrade] = []
+    var symbolsOfOverTenAfterTrades: [String] = []
+    var maxPriceTrade: CoinTrade = CoinTrade(symbol: "", price: nil, volume: 0)
+    var errorMessage: String = ""
+    var sumOfFirstThreeTradeVolume: Int = 0
+    
+    var cancellable = Set<AnyCancellable>()
     
     init() {
         ds = DataService()
-        // 구독
+        firstSubscription()
+        secondSubscription()
+        thirdSubscription()
         ds.streamingData()
     }
     
-    private func firstSubscription(ds: DataService) {
+    private func firstSubscription() {
         ds.publisher
             .drop { $0.volume < 10 }
             .map { $0.symbol }
-            .sink
+            .collect()
+            .sink { _ in
+            } receiveValue: { [weak self] tradeSymbols in
+                guard let self else {return}
+                self.symbolsOfOverTenAfterTrades = tradeSymbols
+            }
+            .store(in: &cancellable)
+    }
+    
+    private func secondSubscription() {
+        ds.publisher
+            .tryMax { trade1, trade2 in
+                guard let first = trade1.price,
+                      let second = trade2.price else {
+                    throw TradeError.priceError
+                }
+                return first < second
+            }
+            .sink { [weak self] completion in
+                guard let self else {return}
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    self.errorMessage = "Error: \(error)"
+                }
+            } receiveValue: { [weak self] maxTrade in
+                guard let self else {return}
+                self.maxPriceTrade = maxTrade
+            }
+            .store(in: &cancellable)
+    }
+    
+    private func thirdSubscription() {
+        ds.publisher
+            .prefix(3)
+            .collect()
+            .sink { _ in
+            } receiveValue: { [weak self] trades in
+                guard let self else {return}
+                self.sumOfFirstThreeTradeVolume = trades.reduce(0, { $0 + $1.volume })
+            }
+            .store(in: &cancellable)
+
     }
 }
--- CoinTrade/MainView.swift
+++ CoinTrade/MainView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @State private var vm: CoinTradeViewModel = .init()
+    
     var body: some View {
-        
-    }
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 20) {
+                Section {
+                    HStack {
+                        VStack(spacing: 10) {
+                            Text("최고가 거래")
+                            Text(vm.maxPriceTrade.symbol)
+                            Text("$\(vm.maxPriceTrade.price?.formatted(.number) ?? "0")")
+                            Spacer()
+                        }
+                        .frame(height: 100)
+                        .frame(maxWidth: .infinity)
+                        VStack(spacing: 10) {
+                            Text("처음 3건 거래")
+                            Text("vol.\(vm.sumOfFirstThreeTradeVolume)")
+                            Spacer()
+                        }
+                        .frame(height: 100)
+                        .frame(maxWidth: .infinity)
+                    }
+                    .font(.title2)
+                }
+            } //:VSTACK
+            .navigationTitle("코인 트레이드")
+            .navigationBarTitleDisplayMode(.large)
+        } //:NAVIGATION
+        .padding()
+    }//:body
 }
 
 #Preview {
